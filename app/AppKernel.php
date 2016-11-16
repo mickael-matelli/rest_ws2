5aa0b99167b9e6e515f2a49553b82469470667d7
mickael-matelli <mickael-matelli@matelli.mg>
2016-11-16 16:08:27 +0300

    socket disabled

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new P2\Bundle\RatchetBundle\P2RatchetBundle(),
+            //new P2\Bundle\RatchetBundle\P2RatchetBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
